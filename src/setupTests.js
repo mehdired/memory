06a01e8904b60acc644a9667169168d7872372db
Mehdi Cheniki <mehdi.cheniki@gmail.com>
2018-04-30 23:09:39 +0200

    .

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,11 +5,15 @@ import createChaiEnzyme from 'chai-enzyme'
 import createChaiJestDiff from 'chai-jest-diff'
 import dirtyChai from 'dirty-chai'
 import sinonChai from 'sinon-chai'
+import chaiJestSnapshot from 'chai-jest-snapshot'
+import enzymeToJSON from 'enzyme-to-json/serializer'
 
 chai
   .use(dirtyChai)
   .use(createChaiJestDiff())
+  .use(chaiJestSnapshot)
   .use(createChaiEnzyme())
   .use(sinonChai)
 
+expect.addSnapshotSerializer(enzymeToJSON)
 configureEnzyme({ adapter: new Adapter() })
